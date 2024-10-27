c3f4b58b007e2212582543be3d68da5e27a2456a
hy5528 <95360525+hy5528@users.noreply.github.com>
2024-10-27 21:15:21 +0800

    Update cms.sh

--- cms.sh
+++ cms.sh
@@ -9,7 +9,7 @@ unzip maccms10.zip
 chmod -R 777 /www/cms
 docker stop cms >/dev/null 2>&1
 docker rm cms >/dev/null 2>&1
-docker network create --subnet 172.19.0.0/16 --gateway 172.19.1.1 --driver bridge film_network
+docker network create --subnet 172.19.0.0/16 --gateway 172.19.0.1 --driver bridge film_network
 docker run -d --name film  --restart=always --user $(id -u):$(id -g) -v /www/cms:/var/www/html  -p 80:80 -e ND_LOGLEVEL=info --network=film_network --ip=172.19.0.2 shinsenter/phpfpm-apache:dev-php7.4 
 docker run -d --name cms --restart=always -p 3306:3306 -v /www/mysql:/var/lib/mysql -e MYSQL_DATABASE=cms -e MYSQL_USER=cms -e MYSQL_PASSWORD=123456 -e MYSQL_ROOT_PASSWORD=123456 --network=film_network --ip=172.19.0.3 yobasystems/alpine-mariadb:10.11
 chmod -R 777 /www
